22edf0cb8ef4b2f88ba042ef52ed29d7c76da5b5
rohithbt <rohitths@gmail.com>
2020-05-15 10:02:08 +0530

    learned var and num

---
+++ num.py
@@ -0,0 +1,9 @@
+A=10  #int
+B=2.64 # float
+C=10+2j # complex numbers
+D=5 + 4j
+
+print(A)
+print(B)
+print(C)
+print(D-C)
---
+++ variable.py
@@ -0,0 +1,18 @@
+A=10
+B="rohith"
+print(A,B)
+
+
+#another example
+
+x,y,z=10,20,30
+print(x,y,z)
+
+
+#another example
+x,y,z=10,20,30
+print(x)
+print(y)
+print(z)
+
+
