a4d35cd5c9168cefabd3ab3637f51299652ee258
rohithbt <rohitths@gmail.com>
2020-05-15 10:09:19 +0530

    learned list

---
+++ tuple.py
@@ -0,0 +1,8 @@
+list=(1,2,3,4)
+print(list)
+print(list[0])  # it will 1 beacuse its in zero index
+print(list[1])
+print(list[2])
+print(list[3])
+
+#list[3]=7 python doesnt not support item assignment
