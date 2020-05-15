4b74cccde91664beb4dcc0c6953f881766f16e90
rohithbt <rohitths@gmail.com>
2020-05-15 10:39:22 +0530

    learned lists

---
+++ list.py
@@ -0,0 +1,19 @@
+A=[]
+A=[1,2,3,4]
+print(A)
+print(A[1])
+print(A[3]) 
+
+
+A[1] = "u can change the list element"
+print(A[1])
+
+
+B=[[1,2,3], 4 ,'third element',(1,2,3,4)] #A list can contain a list inside as well as a tuple
+print(B)
+print(B[3][3])
+
+#lists are enclosed with  brackets[] and tuples with paranthesis()
+#lists are mutable
+#tuples are immutable
+#tuples are faster than lists
