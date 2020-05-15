447f3d5d7db4ecf8b99fa2adbcb673215d8f7e4d
rohithbt <rohitths@gmail.com>
2020-05-15 09:24:32 +0530

    helloworld

---
+++ comments.py
@@ -0,0 +1,8 @@
+# this is the way to single commit write #
+
+'''
+suppose you want to commit more code then add 3 lines
+bulk comment
+or 
+multiline comment
+'''
---
+++ hello.py
@@ -0,0 +1,5 @@
+print("hello world")
+
+print("welcome to edureka")
+
+print("Happy leaning \n welcome to edureka")
