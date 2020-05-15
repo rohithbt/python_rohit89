e87d65a0d99a0fde13e0b038bae0688e243e621d
rohithbt <rohitths@gmail.com>
2020-05-15 10:58:01 +0530

    learned dict

---
+++ dict.py
@@ -0,0 +1,16 @@
+# dictionary contains key value pairs
+
+A={'Age':24,'name':'rohith'}
+print(A['Age'])
+print(A['name'])
+# this is way more readbale
+#here values can be anything like dictionary ,array ,tuple
+
+
+#another way
+
+a=[32,'john']
+print(a[0])
+print(a[1])
+
+#this is not in readable format
