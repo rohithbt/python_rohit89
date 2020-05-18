e339a83b31016763e0ceeede7db8ade797418743
rohithbt <rohitths@gmail.com>
2020-05-18 10:38:12 +0530

    learned for loops

---
+++ for.py
@@ -0,0 +1,12 @@
+fruits=['banana','carrot','putchattni']
+
+for fruit in fruits:
+    print(fruit)
+
+
+
+
+list=[1,2,3,'banana']
+
+for i in list:
+    print(i)
---
+++ nestedfor.py
@@ -0,0 +1,24 @@
+count=1
+for i in range(10):
+    print(str(i)*i)
+    for j in range(0,i):
+        count=count+1
+
+
+#while day == 'thursday':
+#        for i in [1,2,3,4]:
+#            print ('something')
+
+
+
+for i in [1,2,3]:
+    pass
+
+print('A')
+
+
+for i in range(1,11):
+    if i ==5:
+        break
+    #continue
+    print (i)
