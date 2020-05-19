7fa5b5ea72c4d12cce2a62a1b3278ba79d46cf85
rohithbt <rohitths@gmail.com>
2020-05-19 19:45:24 +0530

    list_examples

--- file_op.py
+++ file_op.py
@@ -1,9 +1,21 @@
 list=["java","python","c"]
-'''print(list[1])
+print(list[1])
 print(list[0:2])
 print(list[-1])
 print(list[-2])
-print(list[-3])'''
+print(list[-3])
 #print(list+["reactjs","c++"])
 #print(list*3)
-print("java" in list,"golang" in list)
+#print("java" in list,"golang" in list)
+#list[1]="aws"
+#print(list)
+#del(list[2])
+#print(list)
+
+#list1=[1,2,3,4,5,'a','b','c']
+#print(list.pop(3))
+#print(list1.remove(3))
+
+#print(list1)
+list=[x**2 for x in [1,2,3,4,5]]
+print(list)
---
+++ list_example.py
@@ -0,0 +1,13 @@
+list=[1,2,3]
+list.append("machine learning")
+print(list)
+
+
+list.extend(['g','h'])
+print(list)
+
+list.insert(1,'scripting')
+print(list)
+
+list.remove(3)
+print(list)
