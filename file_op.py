17512a1bf0ddecce37b8c0f23b64a0d353725b3b
rohithbt <rohitths@gmail.com>
2020-05-19 17:37:10 +0530

    file_operations

---
+++ file_op.py
@@ -0,0 +1,9 @@
+list=["java","python","c"]
+'''print(list[1])
+print(list[0:2])
+print(list[-1])
+print(list[-2])
+print(list[-3])'''
+#print(list+["reactjs","c++"])
+#print(list*3)
+print("java" in list,"golang" in list)
